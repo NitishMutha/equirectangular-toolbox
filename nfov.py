397866289a1343e1cf437f5ca9ff23ad0942255b
Nitish Mutha <nitishmutha@gmail.com>
2017-08-24 21:39:51 +0100

    nfov inital comming

---
+++ nfov.py
@@ -0,0 +1,109 @@
+from math import pi
+import numpy as np
+import imageio as im
+
+class NFOV():
+    def __init__(self, height=400, width=800):
+        self.FOV = [0.45, 0.45]
+        self.PI = pi
+        self.PI_2 = pi * 0.5
+        self.PI2 = pi * 2.0
+        self.height = height
+        self.width = width
+        self.screen_points = self._get_screen_img()
+
+    def _get_coord_rad(self, isCenterPt, center_point=None):
+        return (center_point * 2 - 1) * np.array([self.PI, self.PI_2]) \
+            if isCenterPt \
+            else \
+            (self.screen_points * 2 - 1) * np.array([self.PI, self.PI_2]) * (
+                np.ones(self.screen_points.shape) * self.FOV)
+
+    def _get_screen_img(self):
+        xx, yy = np.meshgrid(np.linspace(0, 1, self.width), np.linspace(0, 1, self.height))
+        return np.array([xx.ravel(), yy.ravel()]).T
+
+    def _calcSphericaltoGnomonic(self, convertedScreenCoord):
+        x = convertedScreenCoord.T[0]
+        y = convertedScreenCoord.T[1]
+
+        rou = np.sqrt(x ** 2 + y ** 2)
+        c = np.arctan(rou)
+        sin_c = np.sin(c)
+        cos_c = np.cos(c)
+
+        lat = np.arcsin(cos_c * np.sin(self.cp[1]) + (y * sin_c * np.cos(self.cp[1])) / rou)
+        lon = self.cp[0] + np.arctan2(x * sin_c, rou * np.cos(self.cp[1]) * cos_c - y * np.sin(self.cp[1]) * sin_c)
+
+        lat = (lat / self.PI_2 + 1.) * 0.5
+        lon = (lon / self.PI + 1.) * 0.5
+
+        return np.array([lon, lat]).T
+
+    def _bilinear_interpolation(self, screen_coord):
+        uf = screen_coord.T[0] * self.frame_width  # long - width
+        vf = screen_coord.T[1] * self.frame_height  # lat - height
+
+        u0 = np.floor(uf).astype(int)  # coord of pixel to bottom left
+        v0 = np.floor(vf).astype(int)
+        u2 = np.add(u0, np.ones(uf.shape).astype(int))  # coords of pixel to top right
+        v2 = np.add(v0, np.ones(vf.shape).astype(int))
+
+        mu = np.subtract(uf, u0)  # fraction of way across pixel
+        mv = np.subtract(vf, v0)
+
+        # Pixel values of four corners
+        x0 = np.mod(u0, self.frame_width)
+        x2 = np.mod(u2, self.frame_width)
+        y0 = np.mod(v0, self.frame_height)
+        y2 = np.mod(v2, self.frame_height)
+
+        base_y0 = np.multiply(y0, self.frame_width)
+        base_y2 = np.multiply(y2, self.frame_width)
+
+        A_idx = np.add(base_y0, x0)
+        B_idx = np.add(base_y2, x0)
+        C_idx = np.add(base_y0, x2)
+        D_idx = np.add(base_y2, x2)
+
+        flat_img = np.reshape(self.frame, [-1, self.frame_channel])
+
+        A = np.take(flat_img, A_idx, axis=0)
+        B = np.take(flat_img, B_idx, axis=0)
+        C = np.take(flat_img, C_idx, axis=0)
+        D = np.take(flat_img, D_idx, axis=0)
+
+        wa = np.multiply(x2 - uf, y2 - vf)
+        wb = np.multiply(x2 - uf, vf - y0)
+        wc = np.multiply(uf - x0, y2 - vf)
+        wd = np.multiply(uf - x0, vf - y0)
+
+        # interpolate
+        AA = np.multiply(A, np.array([wa, wa, wa]).T)
+        BB = np.multiply(B, np.array([wb, wb, wb]).T)
+        CC = np.multiply(C, np.array([wc, wc, wc]).T)
+        DD = np.multiply(D, np.array([wd, wd, wd]).T)
+        nfov = np.reshape(np.round(AA + BB + CC + DD).astype(np.uint8), [self.height, self.width, 3])
+        import matplotlib.pyplot as plt
+        plt.imshow(nfov)
+        plt.show()
+        return nfov
+
+    def toNFOV(self, frame, center_point):
+        self.frame = frame
+        self.frame_height = frame.shape[0]
+        self.frame_width = frame.shape[1]
+        self.frame_channel = frame.shape[2]
+
+        self.cp = self._get_coord_rad(center_point=center_point, isCenterPt=True)
+        convertedScreenCoord = self._get_coord_rad(isCenterPt=False)
+        spericalCoord = self._calcSphericaltoGnomonic(convertedScreenCoord)
+        return self._bilinear_interpolation(spericalCoord)
+
+
+# test the class
+if __name__ == '__main__':
+    img = im.imread('images/360.jpg')
+    nfov = NFOV()
+    center_point = np.array([.5, .5])
+    nfov.toNFOV(img, center_point)
